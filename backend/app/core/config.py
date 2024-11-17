0c8ad9b7b096239ec64e2ab1ea6487d780b21627
QING <qlear@qq.com>
2024-11-18 00:44:52 +0800

    build: 增加 Dockerfile 并改进了获取配置的自动模式更换

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,20 +1,23 @@
 from pydantic_settings import BaseSettings
 import os
+from dotenv import load_dotenv
+
+# 加载 .env 配置文件
+load_dotenv()
 
 
 class SDBaseSettings(BaseSettings):
+    APP_ENV: str = "dev"
     APP_NAME: str = "Seeker Drive API"
     API_STR: str = "/api"
-    SECRET_KEY: str = "seekerdrive"
+    SECRET_KEY: str = "seek2geek"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7  # 一周
-    # 静态文件目录，默认为项目根目录下的static文件夹
     STATIC_DIR: str = os.path.join(
         os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "static"
-    )
-    # 文件存储目录，默认为项目根目录下的drive文件夹
+    )  # 静态文件目录，默认为项目根目录下的static文件夹
     DRIVE_DIR: str = os.path.join(
         os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "drive"
-    )
+    )  # 文件存储目录，默认为项目根目录下的drive文件夹
 
     class Config:
         env_file = ".env"
@@ -33,13 +36,12 @@ class ProdSettingsSD(SDBaseSettings):
     ENV: str = "prod"
     DEBUG: bool = False
     TESTING: bool = False
-    DATABASE_URL: str = "sqlite:///./prod.db"
+    DATABASE_URL: str = "sqlite:///./data.db"
     LOGGING_LEVEL: str = "INFO"
 
 
-# 根据传参获取配置
-def get_settings(env: str = "dev"):
-    if env == "prod":
-        return ProdSettingsSD()
-    else:
-        return DevSettingsSD()
+# 根据 .env 文件中的 APP_ENV 获取配置
+def get_settings():
+    env = os.getenv("APP_ENV", "dev")
+    settings_class = ProdSettingsSD if env == "prod" else DevSettingsSD
+    return settings_class()
